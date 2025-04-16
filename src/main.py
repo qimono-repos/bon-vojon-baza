3b8befd124aca0ebc19ef7b6c874dfe5917b7a3d
root <you@example.com>
2025-04-16 00:40:43 +0000

    feat: add component for chart

--- src/main.py
+++ src/main.py
@@ -6,11 +6,166 @@ async def main(page: ft.Page):
 
     async def manage_fab(e):
         print('FAB')                                                                                                                                                                                             
-        page.floating_action_button = ft.FloatingActionButton(
-            text= str('+'),                                                                                                                                                                                                      icon=None,
-            # ft.Icons.AIRPLAY,
-            on_click=manage_fab
-            )
+    
+    page.floating_action_button = ft.FloatingActionButton(
+        text= str('+'),                                                                                                                                                                                                      
+        icon=None,
+        # ft.Icons.AIRPLAY,
+        on_click=manage_fab
+    )
+
+   
+def line_chart():
+    class State:
+        toggle = True
+
+    s = State()
+
+    data_1 = [
+        ft.LineChartData(
+            data_points=[
+                ft.LineChartDataPoint(1, 1),
+                ft.LineChartDataPoint(3, 1.5),
+                ft.LineChartDataPoint(5, 1.4),
+                ft.LineChartDataPoint(7, 3.4),
+                ft.LineChartDataPoint(10, 2),
+                ft.LineChartDataPoint(12, 2.2),
+                ft.LineChartDataPoint(13, 1.8),
+            ],
+            stroke_width=8,
+            color=ft.colors.LIGHT_GREEN,
+            curved=True,
+            stroke_cap_round=True,
+        ),
+        ft.LineChartData(
+            data_points=[
+                ft.LineChartDataPoint(1, 1),
+                ft.LineChartDataPoint(3, 2.8),
+                ft.LineChartDataPoint(7, 1.2),
+                ft.LineChartDataPoint(10, 2.8),
+                ft.LineChartDataPoint(12, 2.6),
+                ft.LineChartDataPoint(13, 3.9),
+            ],
+            color=ft.colors.PINK,
+            below_line_bgcolor=ft.colors.with_opacity(0, ft.colors.PINK),
+            stroke_width=8,
+            curved=True,
+            stroke_cap_round=True,
+        ),
+        ft.LineChartData(
+            data_points=[
+                ft.LineChartDataPoint(1, 2.8),
+                ft.LineChartDataPoint(3, 1.9),
+                ft.LineChartDataPoint(6, 3),
+                ft.LineChartDataPoint(10, 1.3),
+                ft.LineChartDataPoint(13, 2.5),
+            ],
+            color=ft.colors.CYAN,
+            stroke_width=8,
+            curved=True,
+            stroke_cap_round=True,
+        ),
+    ]
+
+    chart = ft.LineChart(
+        data_series=data_1,
+        border=ft.Border(
+            bottom=ft.BorderSide(4, ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE))
+        ),
+        left_axis=ft.ChartAxis(
+            labels=[
+                ft.ChartAxisLabel(
+                    value=1,
+                    label=ft.Text("1m", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=2,
+                    label=ft.Text("2m", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=3,
+                    label=ft.Text("3m", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=4,
+                    label=ft.Text("4m", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=5,
+                    label=ft.Text("5m", size=14, weight=ft.FontWeight.BOLD),
+                ),
+                ft.ChartAxisLabel(
+                    value=6,
+                    label=ft.Text("6m", size=14, weight=ft.FontWeight.BOLD),
+                ),
+            ],
+            labels_size=40,
+        ),
+        bottom_axis=ft.ChartAxis(
+            labels=[
+                ft.ChartAxisLabel(
+                    value=2,
+                    label=ft.Container(
+                        ft.Text(
+                            "Monday",
+                            size=16,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                ),
+                ft.ChartAxisLabel(
+                    value=7,
+                    label=ft.Container(
+                        ft.Text(
+                            "Tuesday",
+                            size=16,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                ),
+                ft.ChartAxisLabel(
+                    ft.Text(
+                        value=12,
+                        label=ft.Container(
+                            "Wednesday",
+                            size=16,
+                            weight=ft.FontWeight.BOLD,
+                            color=ft.colors.with_opacity(0.5, ft.colors.ON_SURFACE),
+                        ),
+                        margin=ft.margin.only(top=10),
+                    ),
+                ),
+            ],
+            labels_size=32,
+        ),
+        tooltip_bgcolor=ft.colors.with_opacity(0.8, ft.colors.BLUE_GREY),
+        min_y=0,
+        max_y=4,
+        min_x=0,
+        max_x=14,
+        # animate=5000,
+        # expand=True,
+        # width=700,
+        # height=500,
+    )
+
+    def toggle_data(e):
+        if s.toggle:
+            pass
+        else:
+            chart.data_series = data_1
+            chart.max_y = 4
+            chart.interactive = True
+        s.toggle = not s.toggle
+        chart.update()
+
+    return ft.Column(
+        controls=[ft.IconButton(ft.icons.REFRESH, on_click=toggle_data), chart],
+    )
 
     container=ft.Container(
         ft.ElevatedButton(text="Say helo to QiMono")
